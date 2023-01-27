ebb8e6ba0c4d71a2c510e027c325be55f201015c
agrigo <agrigo@netcompany-intrasoft.com>
2023-01-27 19:32:31 +0200

    Routing implement

--- web-ui/src/Routes/Routing.tsx
+++ web-ui/src/Routes/Routing.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import {Routes, Route} from 'react-router-dom'
-import DataTable from "../Components/DataTable";
+import StockInformation from "../Components/StockInformation";
+import StockFinancial from "../Components/StockFinancial";
 
 const Routing = () => {
     return (<Routes>
-        <Route path={'/'} element={<DataTable/>}/>
+        <Route path={'/'} element={<StockInformation/>}/>
+        <Route path={'/stock-information'} element={<StockInformation/>}/>
+        <Route path={'/stock-financials'} element={<StockFinancial/>}/>
     </Routes>)
 
 }
